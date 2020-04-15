aa50aa006cb721c1053c51016f6d49df673111e6
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-04-15 01:55:25 -0500

    Updates

---
+++ precache-manifest.90b1e71a1ca8d3ef688f8fbd4b78262c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aea2bce809b3d71d58e5dd2ae0c3c11",
+    "url": "/simple-hangman.github.io/index.html"
+  },
+  {
+    "revision": "54fcab0f1dbac32eff48",
+    "url": "/simple-hangman.github.io/static/css/main.5f3c01bb.chunk.css"
+  },
+  {
+    "revision": "09dbb4503941fc33d284",
+    "url": "/simple-hangman.github.io/static/js/2.b9c52705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-hangman.github.io/static/js/2.b9c52705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54fcab0f1dbac32eff48",
+    "url": "/simple-hangman.github.io/static/js/main.d35aa77b.chunk.js"
+  },
+  {
+    "revision": "3e3229c8fe4b2bbf1e20",
+    "url": "/simple-hangman.github.io/static/js/runtime-main.53df5c03.js"
+  }
+]);
