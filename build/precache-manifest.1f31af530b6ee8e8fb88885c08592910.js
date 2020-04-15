f8ce08276347cc6a85695f0a03e9c571e1964052
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-04-15 01:37:23 -0500

    first part done

---
+++ build/precache-manifest.1f31af530b6ee8e8fb88885c08592910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd00fa6b4e153f395c8bad529864b40a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54fcab0f1dbac32eff48",
+    "url": "/static/css/main.5f3c01bb.chunk.css"
+  },
+  {
+    "revision": "09dbb4503941fc33d284",
+    "url": "/static/js/2.b9c52705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b9c52705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54fcab0f1dbac32eff48",
+    "url": "/static/js/main.d35aa77b.chunk.js"
+  },
+  {
+    "revision": "b094cf9971df9d29d775",
+    "url": "/static/js/runtime-main.c4853cca.js"
+  }
+]);
